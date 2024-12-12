e76e1dfac4e3f799b385530ec09192f230114520
FuxuanNet <2639364698@qq.com>
2024-12-12 18:18:50 +0800

    改了改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ['vue3-markdown']
+    include: ['vue3-markdown'], // 如果在项目中使用了该依赖
   },
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue(), // 使用 Vue 插件
+    process.env.NODE_ENV === 'development' && require('vite-plugin-vue-devtools')(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 配置别名
     },
   },
-})
+});
